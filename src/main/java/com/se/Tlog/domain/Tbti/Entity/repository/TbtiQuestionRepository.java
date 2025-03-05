ae09f98a7a8bb02898caa0997471b8296a4b4a96
gojungsu <gjs4565@naver.com>
2025-03-05 14:36:58 +0900

    feat: TbtiQuestion 키 uuid로 변경으로 인한 수정

--- src/main/java/com/se/Tlog/domain/Tbti/Entity/repository/TbtiQuestionRepository.java
+++ src/main/java/com/se/Tlog/domain/Tbti/Entity/repository/TbtiQuestionRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TbtiQuestionRepository extends JpaRepository<TbtiQuestion,Long> {
+import java.util.UUID;
+
+public interface TbtiQuestionRepository extends JpaRepository<TbtiQuestion, UUID> {
     Page<TbtiQuestion> findByTraitCategory(TraitCategory traitCategory, Pageable pageable);
 }
