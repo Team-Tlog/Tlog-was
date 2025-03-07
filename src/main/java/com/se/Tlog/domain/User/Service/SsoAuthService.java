cf09725efa85acc3e1e7c12919b3b456897cced2
gojungsu <gjs4565@naver.com>
2025-03-07 22:02:08 +0900

    feat: refreshToken redis저장하는 코드 작성

--- src/main/java/com/se/Tlog/domain/User/Service/SsoAuthService.java
+++ src/main/java/com/se/Tlog/domain/User/Service/SsoAuthService.java
@@ -9,6 +9,8 @@ import com.se.Tlog.domain.User.repository.UserRepository;
 import com.se.Tlog.global.exception.CustomException;
 import com.se.Tlog.global.response.error.ErrorType;
 import com.se.Tlog.global.util.jwt.JwtUtil;
+import com.se.Tlog.global.util.redis.RedisProperties;
+import com.se.Tlog.global.util.redis.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,7 @@ public class SsoAuthService {
     private final Map<SsoType, SsoService> ssoServiceMap;
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
+    private final RedisUtil redisUtil;
 
     public TokenDto login(LoginRequest loginRequest){
         SsoService ssoService = Optional.ofNullable(ssoServiceMap.get(loginRequest.type()))
@@ -35,6 +38,8 @@ public class SsoAuthService {
 
         String accessToken = jwtUtil.generateAccessToken(user.getId().toString(), user.getRole().getValue());
         String refreshToken = jwtUtil.generateRefreshToken(user.getId().toString(), user.getRole().getValue());
+
+        redisUtil.save(RedisProperties.REFRESH_TOKEN_PREFIX + user.getId(), refreshToken, jwtUtil.getRefreshTokenDuration());
 
         return TokenDto.builder()
                 .accessToken(accessToken)
