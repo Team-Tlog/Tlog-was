2269f440e65619d7811324b2f0b3b89e68699e06
joongsu <112760801+joongsu@users.noreply.github.com>
2025-03-07 23:24:20 +0900

    Feature: Redis 환경 set (#30)
    * feat: redis 사용을 위한 yml 설정
    
    * feat: 테스트를 위한 로그 확인 코드 추가
    
    * feat: 레디스 의존성 추가
    
    * feat: RedisConfig 작성
    
    * feat: RedisProperties 작성
    
    * feat: jwtutil 메소드 추가 작성
    
    * feat: Redisutil 작성
    
    * feat: refreshToken redis저장하는 코드 작성

---
+++ src/main/java/com/se/Tlog/global/util/redis/RedisProperties.java
@@ -0,0 +1,6 @@
+package com.se.Tlog.global.util.redis;
+
+public interface RedisProperties {
+    String REFRESH_TOKEN_PREFIX = "refresh_token:";
+    String ACCESS_TOKEN_PREFIX = "access_token:";
+}
