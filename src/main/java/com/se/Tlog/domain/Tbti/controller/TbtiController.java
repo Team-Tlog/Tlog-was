18a811ff5e856d90fae10f70ff48c6f4064e66f8
patternplane <bss011202@naver.com>
2025-03-08 12:12:12 +0900

    feat: 예시 - API별 명세 작성
    - summary, description : 기본 설명 포함
    - tags : API끼리 묶어 볼 수 있음
    - security : API에서 요구되는 인증/인가 관련 설정
    - parameters : 입력 파라미터 명세
    - responseBody : Http Body로 전송되는 데이터 명세
    - responses : Http 응답 및 응답 데이터 명세

--- src/main/java/com/se/Tlog/domain/Tbti/controller/TbtiController.java
+++ src/main/java/com/se/Tlog/domain/Tbti/controller/TbtiController.java
@@ -1,9 +1,16 @@
 package com.se.Tlog.domain.Tbti.controller;
 
 import com.se.Tlog.domain.Tbti.dto.TbtiQuestionReq;
+import com.se.Tlog.domain.Tbti.dto.TbtiQuestionResponse;
 import com.se.Tlog.domain.Tbti.service.TbtiService;
 import com.se.Tlog.global.response.success.SuccessRes;
 import com.se.Tlog.global.response.success.SuccessType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -21,6 +28,25 @@ public class TbtiController {
     final TbtiService tbtiService;
 
     @GetMapping
+    @Operation (
+    		summary = "전체 TBTI 질문 가져오기",
+    		description = "모든 TBTI 질문을 반환합니다.",
+			tags = {"TBTI 도메인"},
+			security = @SecurityRequirement(
+					name = "JwtAuthScheme", // OpenApiConfig에 설정된 Security Scheme 이름일 것
+					scopes = {"scope1", "scope2"}),
+			parameters = {
+					@Parameter(name = "categories", description = "TBTI 특성 카테고리입니다."),
+					@Parameter(name = "pageable", description = "Page 정보")
+    		},
+			responses = {
+					@ApiResponse( responseCode = "200", description = "처리 성공시 반환되는 TBTI 질문 Page입니다.",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = TbtiQuestionResponse.class)) // Generic 반환의 경우, 정확한 표시를 위해서는 별도의 class로 재정의 필요
+					), 
+					@ApiResponse(responseCode = "505", description = "서버 내부 오류")}
+	)
     public ResponseEntity<?> getAllTbtiQuestion(
             @RequestParam(name = "categories", required = false) String traitCategory,
             @PageableDefault(size = 10) Pageable pageable
@@ -31,13 +57,37 @@ public class TbtiController {
     }
 
     @PostMapping
+    @Operation (
+    		summary = "새 TBTI 질문 등록하기",
+    		description = "새 TBTI 질문을 등록합니다.",
+			tags = {"TBTI 도메인"},
+			security = @SecurityRequirement(
+					name = "JwtAuthScheme", // OpenApiConfig에 설정된 Security Scheme 이름일 것
+					scopes = {"scope1", "scope2"}),
+			responses = {
+					@ApiResponse( responseCode = "200", description = "성공"), 
+					@ApiResponse(responseCode = "505", description = "서버 내부 오류")}
+	)
     public ResponseEntity<?> createTbtiQuestion(@RequestBody TbtiQuestionReq tbtiQuestionReq){
         tbtiService.createTbtiQuestion(tbtiQuestionReq);
 
         return ResponseEntity.ok()
                 .body(SuccessRes.from(SuccessType.OK));
     }
+    
     @DeleteMapping("/{tbtiQuestionId}")
+    @Operation (
+    		summary = "TBTI 질문 삭제하기",
+    		description = "TBTI 질문을 삭제합니다.",
+			tags = {"TBTI 도메인"},
+			security = @SecurityRequirement(
+					name = "JwtAuthScheme", // OpenApiConfig에 설정된 Security Scheme 이름일 것
+					scopes = {"scope1", "scope2"}),
+			parameters = {@Parameter(name = "tbtiQuestionId", description = "삭제할 TBTI 질문의 UUID입니다.")},
+			responses = {
+					@ApiResponse( responseCode = "200", description = "성공"), 
+					@ApiResponse(responseCode = "505", description = "서버 내부 오류")}
+	)
     public ResponseEntity<?> deleteTbtiQuestion(@PathVariable(name = "tbtiQuestionId") UUID tbtiQuestionId) {
         tbtiService.deleteTbtiQuestion(tbtiQuestionId);
         return ResponseEntity.ok()
