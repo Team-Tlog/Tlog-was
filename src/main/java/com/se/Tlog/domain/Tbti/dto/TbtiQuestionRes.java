a36a908e1804315c0b4612133a3bfdc2af879047
patternplane <bss011202@naver.com>
2025-03-08 11:38:54 +0900

    feat: 예시 - 데이터 형식 명세 (Schemas)
    - Swagger UI에서 표시되는 데이터 스키마의 명세

--- src/main/java/com/se/Tlog/domain/Tbti/dto/TbtiQuestionRes.java
+++ src/main/java/com/se/Tlog/domain/Tbti/dto/TbtiQuestionRes.java
@@ -3,12 +3,20 @@ package com.se.Tlog.domain.Tbti.dto;
 import com.se.Tlog.domain.Tbti.Entity.TbtiQuestion;
 import com.se.Tlog.domain.Tbti.Entity.TraitCategory;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.UUID;
 
-
+@Schema(description = "TBTI 질문 응답 DTO")
 public record TbtiQuestionRes(
+		
+		@Schema(description = "TBTI 질문의 고유 UUID")
         UUID id,
+        
+        @Schema(description = "TBTI 질문의 표시 내용")
         String content,
+        
+        @Schema(description = "질문지의 TBTI 영역")
         TraitCategory traitCategory
 ) {
     public static TbtiQuestionRes from(TbtiQuestion tbtiQuestion) {
