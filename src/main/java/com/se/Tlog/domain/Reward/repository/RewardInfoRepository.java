d234df384e6753e8497c09e6a8b881fc872063a4
patternplane <bss011202@naver.com>
2025-03-10 18:00:49 +0900

    feat: RewardInfoRepository 작성

---
+++ src/main/java/com/se/Tlog/domain/Reward/repository/RewardInfoRepository.java
@@ -0,0 +1,9 @@
+package com.se.Tlog.domain.Reward.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.se.Tlog.domain.Reward.Entity.RewardInfo;
+
+public interface RewardInfoRepository extends JpaRepository<RewardInfo, Long> {
+
+}
