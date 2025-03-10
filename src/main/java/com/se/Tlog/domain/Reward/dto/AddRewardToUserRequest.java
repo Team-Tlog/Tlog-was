1290c13f0ac3a2330c8d40a353c1fef0d01c61be
patternplane <bss011202@naver.com>
2025-03-10 17:55:16 +0900

    feat: AddRewardToUserRequest DTO 작성

---
+++ src/main/java/com/se/Tlog/domain/Reward/dto/AddRewardToUserRequest.java
@@ -0,0 +1,15 @@
+package com.se.Tlog.domain.Reward.dto;
+
+import java.util.UUID;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "특정 유저에게 보상을 지급하도록 요청하는 데이터 규격")
+public record AddRewardToUserRequest(
+		@Schema(description = "보상을 지급할 유저")
+		UUID userId,
+		
+		@Schema(description = "적용할 보상")
+		Long rewardInfoId) {
+	
+}
