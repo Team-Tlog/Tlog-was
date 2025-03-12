443a0814e8e8c74f1b8539eb8d24610a58907109
gojungsu <gjs4565@naver.com>
2025-03-13 04:01:53 +0900

    feat: spring 혼동으로 인한 jpa,mongo repsoitory 패키지 분리와 환경설정

---
+++ tlog-was/src/main/java/com/se/Tlog/config/DataSourceConfig.java
@@ -0,0 +1,15 @@
+package com.se.Tlog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.se.Tlog.domain.**.repository.jpa"
+)
+@EnableMongoRepositories(
+        basePackages = "com.se.Tlog.domain.**.repository.mongo"
+)
+public class DataSourceConfig {
+}
