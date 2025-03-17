1e8de597bd17551f5e4d1ddffddd955ffc133153
gojungsu <gjs4565@naver.com>
2025-03-17 20:40:17 +0900

    feat: followDomain dto 작성

---
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/presentation/dto/FollowStatusDto.java
@@ -0,0 +1,11 @@
+package com.se.Tlog.domain.Social.presentation.dto;
+
+public record FollowStatusDto(
+        boolean status,
+        String message
+){
+    public static FollowStatusDto from(boolean status) {
+        String msg = status ? "팔로우 되었습니다." : "언팔로우 되었습니다.";
+        return new FollowStatusDto(status,msg);
+    }
+}
