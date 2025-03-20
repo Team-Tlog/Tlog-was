1faae54c4592fb72d59ddbaf0fc3101a46381519
gojungsu <gjs4565@naver.com>
2025-03-20 15:18:03 +0900

    feat: ChatController 작성

---
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/controller/ChatController.java
@@ -0,0 +1,33 @@
+package com.se.Tlog.domain.Social.Chat.controller;
+
+import com.se.Tlog.domain.Social.Chat.controller.dto.ChatMessageRequestDto;
+import com.se.Tlog.domain.Social.Chat.domain.ChatMessage;
+import com.se.Tlog.domain.Social.Chat.service.ChatService;
+import com.se.Tlog.domain.Social.Chat.service.RedisPublisher;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+    private final ChatService chatService;
+    private final RedisPublisher redisPublisher;
+
+    @MessageMapping("/chat/message")
+    public void message(@Payload ChatMessageRequestDto chatMessageRequestDto) {
+        if (chatMessageRequestDto.senderId() == null || chatMessageRequestDto.chatRoomId() == null) {
+            log.warn("Invalid message payload: {}", chatMessageRequestDto);
+            throw new IllegalArgumentException("senderId와 chatRoomId는 필수입니다.");
+        }
+
+        ChatMessage message = chatService.createMessage(chatMessageRequestDto);
+        redisPublisher.publish(new ChannelTopic("chat"),message);
+    }
+
+}
