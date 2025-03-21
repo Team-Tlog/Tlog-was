13d08a31f47b198eac113992648c5399071dd7fd
gojungsu <gjs4565@naver.com>
2025-03-21 19:07:49 +0900

    refact: 빠진 어노테이션 추가

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/repository/jpa/ChatRoomUserRepository.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/repository/jpa/ChatRoomUserRepository.java
@@ -2,8 +2,9 @@ package com.se.Tlog.domain.Social.Chat.repository.jpa;
 
 import com.se.Tlog.domain.Social.Chat.domain.ChatRoomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface ChatRoomUserRepository extends JpaRepository<ChatRoomUser, UUID> {
 }
