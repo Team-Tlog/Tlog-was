89d9c5b0616a5204b560377b6b51acb2b32fc54e
gojungsu <gjs4565@naver.com>
2025-03-25 16:07:53 +0900

    feat: 읽음 처리 필요 dto 수정

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/controller/dto/ChatMessageReadDto.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/controller/dto/ChatMessageReadDto.java
@@ -2,8 +2,8 @@ package com.se.Tlog.domain.Social.Chat.controller.dto;
 
 import java.util.UUID;
 
-public record ChatMessageCheckDto(
+public record ChatMessageReadDto(
         UUID readerId,
-        Long messageId
+        Long lastReadMessageId
 ) {
 }
