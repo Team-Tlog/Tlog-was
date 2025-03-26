7c37f1f1e954efe86e0c8bc81a432e7290719ab5
patternplane <bss011202@naver.com>
2025-03-26 22:58:32 +0900

    feat: Team 엔티티 작성

---
+++ tlog-was/src/main/java/com/se/Tlog/domain/Team/domain/Team.java
@@ -0,0 +1,73 @@
+package com.se.Tlog.domain.Team.domain;
+
+import java.util.UUID;
+
+import com.se.Tlog.domain.Team.domain.repository.TeamRepositoryService;
+import com.se.Tlog.domain.User.domain.User;
+import com.se.Tlog.global.exception.CustomException;
+import com.se.Tlog.global.response.error.ErrorType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Team {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	
+	private Long inviteCode;
+	
+	private String name;
+	
+	// private Tbti tbti;
+	
+	private Team(String name, Long inviteCode) {
+		this.name = name;
+		this.inviteCode = inviteCode;
+	}
+	
+	public static Team create(
+			String name, 
+			Long inviteCode) {
+		if (name == null || name.isBlank())
+			throw new CustomException(ErrorType.TEAM_NAME_NOT_FOUND);
+		if (!InviteCodeUtil.isValidCode(inviteCode))
+			throw new CustomException(ErrorType.INTERNAL_ERROR_BY_INVITE_CODE);
+		
+		Team newTeam = new Team(name, inviteCode);
+		return newTeam;
+	}
+	
+	public void addUser(User user, TeamRepositoryService repoService) {
+		if (repoService.isExistInTeam(this.getId(), user.getId()))
+			throw new CustomException(ErrorType.ALREADY_EXIST_IN_TEAM);
+		
+		repoService.addUserToTeam(id, user.getId());
+		// 기타 팀원 추가시 처리내용
+		
+		log.info("팀원을 팀 " + this.name + "에 추가합니다. : " + user.getName());
+	}
+	
+	public void deleteUser(User user, TeamRepositoryService repoService) {
+		if (!repoService.isExistInTeam(this.id, user.getId()))
+			throw new CustomException(ErrorType.TEAM_USER_NOT_FOUND);
+		if (repoService.countMemberInTeam(id) == 1)
+			throw new CustomException(ErrorType.TEAM_CANNOT_BE_ORPHAN);
+		
+		repoService.deleteUserInTeam(this.id, user.getId());
+		// 기타 팀원 삭제 후 처리내용
+		
+		log.info("팀원을 팀 " + this.name + "에서 제거합니다. : " + user.getName());
+	}
+}
