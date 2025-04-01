dfa4f4d8c96f5e8f5382cdc393a9388b65df9c0c
gojungsu <gjs4565@naver.com>
2025-04-01 22:13:08 +0900

    feat: 채팅방 리스트 받을 때 필요한 dto 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/controller/dto/ChatRoomResponseDto.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/controller/dto/ChatRoomResponseDto.java
@@ -1,12 +1,20 @@
 package com.se.Tlog.domain.Social.Chat.controller.dto;
 
+import com.se.Tlog.domain.User.domain.User;
+
+import java.util.List;
 import java.util.UUID;
 
 public record ChatRoomResponseDto(
         Long roomId,
-        UUID hostId
+        UUID hostId,
+        List<ChatUserSummaryDto> chatUserSummaryDtoList
 ) {
-    public static ChatRoomResponseDto from(Long roomId, UUID hostId) {
-        return new ChatRoomResponseDto(roomId, hostId);
+    public static ChatRoomResponseDto from(Long roomId, UUID hostId, List<User> users) {
+        return new ChatRoomResponseDto(
+                roomId,
+                hostId,
+                users.stream().map(ChatUserSummaryDto::from).toList()
+        );
     }
 }
