34be157b44f81cb98602c7c757dc5f94712c5004
gojungsu <gjs4565@naver.com>
2025-04-07 21:04:30 +0900

    refact: 불필요한 필드 삭제

--- tlog-was/src/main/java/com/se/Tlog/domain/Travel/controller/dto/TagRes.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Travel/controller/dto/TagRes.java
@@ -2,10 +2,9 @@ package com.se.Tlog.domain.Travel.controller.dto;
 
 public record TagRes(
         String tagId,
-        String tagName,
-        boolean deleted
+        String tagName
 ) {
-    public static TagRes from(String tagId, String tagName, boolean deleted) {
-        return new TagRes(tagId, tagName, deleted);
+    public static TagRes from(String tagId, String tagName) {
+        return new TagRes(tagId, tagName);
     }
 }
