fbf6970bb1bfdcf2de91dc40b21d5fd273bb6441
gojungsu <gjs4565@naver.com>
2025-04-07 20:55:58 +0900

    feat: isDeleted:false 를 포함하지 않는 document 제외하는 쿼리 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Travel/repository/mongo/DestinationRepository.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Travel/repository/mongo/DestinationRepository.java
@@ -1,15 +1,20 @@
 package com.se.Tlog.domain.Travel.repository.mongo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.se.Tlog.domain.Travel.domain.Destination;
 
-import java.util.List;
 
 @Repository
 public interface DestinationRepository extends MongoRepository<Destination, String> {
-    List<Destination> findByTagsIdContains(String tagId);
 
     boolean existsByName(String name);
+
+    @Query("{'tags.isDeleted' :  false}")
+    Page<Destination> findAllWithActiveTags(Pageable pageable);
+
 }
