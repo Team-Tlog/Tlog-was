83cd9973161178e24a73148e9f4719dfbb1c7aad
gojungsu <gjs4565@naver.com>
2025-04-07 20:56:24 +0900

    feat: destination 반환하는 Res 작ㄱ성

---
+++ tlog-was/src/main/java/com/se/Tlog/domain/Travel/controller/dto/DestinationRes.java
@@ -0,0 +1,33 @@
+package com.se.Tlog.domain.Travel.controller.dto;
+
+import com.se.Tlog.domain.Travel.domain.Destination;
+import com.se.Tlog.domain.Travel.domain.Location;
+
+import java.util.List;
+
+public record DestinationRes(
+         String name,
+         String address,
+         Location location,
+         int rating,
+         String city,
+         boolean hasParking,
+         boolean petFriendly,
+         List<TagIdDto>tags
+) {
+    public static DestinationRes from(Destination destination) {
+        List<TagIdDto> tagIdDtoList = destination.getTags().stream().filter(tagInfo -> !tagInfo.isDeleted())
+                .map(tagInfo -> TagIdDto.from(tagInfo.getId(), tagInfo.getWeight()))
+                .toList();
+        return new DestinationRes(
+                destination.getName(),
+                destination.getAddress(),
+                destination.getLocation(),
+                destination.getRating(),
+                destination.getCity(),
+                destination.isHasParking(),
+                destination.isPetFriendly(),
+                tagIdDtoList
+        );
+    }
+}
