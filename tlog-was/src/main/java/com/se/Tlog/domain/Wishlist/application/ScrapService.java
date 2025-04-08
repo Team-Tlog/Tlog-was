f2419e604f27243ae17abd6ea13372050674efca
patternplane <bss011202@naver.com>
2025-04-08 22:23:34 +0900

    feat: ScrapService 작성

---
+++ tlog-was/src/main/java/com/se/Tlog/domain/Wishlist/application/ScrapService.java
@@ -0,0 +1,47 @@
+package com.se.Tlog.domain.Wishlist.application;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.se.Tlog.domain.ApplicationService;
+import com.se.Tlog.domain.Travel.controller.dto.DestinationRes;
+import com.se.Tlog.domain.Wishlist.domain.OwnerType;
+import com.se.Tlog.domain.Wishlist.domain.WishlistService;
+import com.se.Tlog.domain.Wishlist.domain.WishlistType;
+import com.se.Tlog.domain.Wishlist.domain.dto.WishlistOwnerDto;
+import com.se.Tlog.domain.Wishlist.domain.UpdateType;
+
+@ApplicationService
+public class ScrapService {
+	@Autowired
+	WishlistService wishlistService;
+	
+	public List<DestinationRes> getScrapData(UUID userId) {
+		return wishlistService.getWishlistData(
+				new WishlistOwnerDto(
+						OwnerType.USER,
+						userId),
+				WishlistType.SCRAP)
+				.stream().map(DestinationRes::from).toList();
+	}
+	
+	private void updateScrap(UpdateType updateType, UUID userId, String destinationId) {
+		wishlistService.updateWishlist(
+				updateType,
+				new WishlistOwnerDto(
+						OwnerType.USER,
+						userId),
+				WishlistType.SCRAP, 
+				destinationId);
+	}
+	
+	public void scrapDestination(UUID userId, String destinationId) {
+		updateScrap(UpdateType.ADD, userId, destinationId);
+	}
+	
+	public void unscrapDescription(UUID userId, String destinationId) {
+		updateScrap(UpdateType.DELETE, userId, destinationId);
+	}
+}
