14b77a5f958e6df4cf89d2be117feabc5b450dc1
patternplane <bss011202@naver.com>
2025-04-10 10:03:12 +0900

    feat: 유저 계정 삭제시 유저 Wishlist 삭제 로직 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/User/domain/service/UserDomainService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/User/domain/service/UserDomainService.java
@@ -3,6 +3,9 @@ package com.se.Tlog.domain.User.domain.service;
 
 import com.se.Tlog.domain.User.controller.dto.UserSummaryDto;
 import com.se.Tlog.domain.User.repository.jpa.UserRepository;
+import com.se.Tlog.domain.Wishlist.domain.OwnerType;
+import com.se.Tlog.domain.Wishlist.domain.WishlistService;
+import com.se.Tlog.domain.Wishlist.domain.dto.WishlistOwnerDto;
 import com.se.Tlog.global.exception.CustomException;
 import com.se.Tlog.global.response.error.ErrorType;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +18,21 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserDomainService {
     private final UserRepository userRepository;
+    private final WishlistService wishlistService;
 
     public List<UserSummaryDto> getUserByIds(List<UUID> uuidList) {
         return userRepository.findAllById(uuidList).stream()
                 .map(UserSummaryDto::from)
                 .toList();
+    }
+    
+    public void deleteUserAccount(UUID userId) {
+    	validateExists(userId);
+    	
+    	// 각종 유저 계정 삭제 처리
+    	wishlistService.deleteWishlist(new WishlistOwnerDto(OwnerType.USER, userId));
+    	
+    	userRepository.deleteById(userId);
     }
 
     public void validateExists(UUID userId) {
