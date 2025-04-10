143d47c17f703765f8a6902bf72a4732fcc8e878
gojungsu <gjs4565@naver.com>
2025-04-10 11:58:49 +0900

    feat: RabbitMqChatProperties 작성 (custom이라 기존과 다르게 처리)

---
+++ tlog-was/src/main/java/com/se/Tlog/config/RabbitMqChatProperties.java
@@ -0,0 +1,16 @@
+package com.se.Tlog.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "spring.custom.rabbitmq.chat")
+public class RabbitMqChatProperties {
+    private String queue;
+    private String exchange;
+    private String routingKey;
+}
