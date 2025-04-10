dbe2b62984ae8d18a58a708aa891c64b38d43ccd
gojungsu <gjs4565@naver.com>
2025-04-10 12:03:16 +0900

    refact: ChatController 불필요한 코드 삭제 및 변경

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/controller/ChatController.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/controller/ChatController.java
@@ -2,12 +2,9 @@ package com.se.Tlog.domain.Social.Chat.controller;
 
 import com.se.Tlog.domain.Social.Chat.controller.dto.ChatMessageReadDto;
 import com.se.Tlog.domain.Social.Chat.controller.dto.ChatMessageRequestDto;
-import com.se.Tlog.domain.Social.Chat.domain.ChatMessage;
 import com.se.Tlog.domain.Social.Chat.service.ChatService;
-import com.se.Tlog.domain.Social.Chat.service.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
@@ -20,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ChatController {
 
     private final ChatService chatService;
-    private final RedisPublisher redisPublisher;
 
     @MessageMapping("/chat/message")
     public void message(@Payload ChatMessageRequestDto chatMessageRequestDto) {
@@ -28,16 +24,11 @@ public class ChatController {
             log.warn("Invalid message payload: {}", chatMessageRequestDto);
             throw new IllegalArgumentException("senderId와 chatRoomId는 필수입니다.");
         }
-
-        ChatMessage message = chatService.sendChatMessage(chatMessageRequestDto);
-        redisPublisher.publish(new ChannelTopic("chat"),message);
+        chatService.sendChatMessage(chatMessageRequestDto);
     }
 
-    @PatchMapping("/chat/room/{roomId}/message")
+    @PatchMapping("/chat/message/read")
     public void checkMessage(@RequestBody ChatMessageReadDto chatMessageCheckDto) {
-
         chatService.checkMessage(chatMessageCheckDto);
     }
-
-
 }
