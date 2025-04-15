7937b11efe38d7491d8f426643aa484c15cbd042
gojungsu <gjs4565@naver.com>
2025-04-16 02:32:57 +0900

    feat: 블랙리스트된 토큰은 api요청 막아주는 로직 필터에 작성

--- tlog-was/src/main/java/com/se/Tlog/global/security/filter/JwtAuthenticationFilter.java
+++ tlog-was/src/main/java/com/se/Tlog/global/security/filter/JwtAuthenticationFilter.java
@@ -6,7 +6,9 @@ import com.se.Tlog.domain.User.repository.jpa.UserRepository;
 import com.se.Tlog.global.response.error.ErrorType;
 import com.se.Tlog.global.security.dto.AdminDetails;
 import com.se.Tlog.global.security.dto.AppUserDetails;
-import com.se.Tlog.global.util.jwt.JwtUtil;
+import com.se.Tlog.global.util.jwt.AccessTokenProvider;
+import com.se.Tlog.global.util.redis.RedisProperties;
+import com.se.Tlog.global.util.redis.RedisUtil;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -30,9 +32,10 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtUtil jwtUtil;
+    private final AccessTokenProvider accessTokenProvider;
     private final UserRepository userRepository;
     private final AdminRepository adminRepository;
+    private final RedisUtil redisUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -42,14 +45,28 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             log.warn("TOKEN NOT FOUND or TOKEN HEADER IS WRONG : " + authorization);
         } else {
             String accessToken = authorization.split(" ")[1];
-            Authentication authToken = getAuthentication(accessToken, response);
+
+            Claims claims = accessTokenProvider.parseToken(accessToken);
+            String jti = claims.get("jti").toString();
+            String accessKey = RedisProperties.ACCESS_TOKEN_PREFIX + jti;
+
+            boolean isBlacklisted = redisUtil.isTokenBlacklisted(accessKey);
+
+            if (isBlacklisted) {
+                log.warn("BLOCKED TOKEN (jti = {})", jti);
+                response.setStatus(ErrorType.UN_AUTHORIZATION.getStatusCode());
+                response.setContentType("application/json");
+                response.getWriter().write("Token is blacklisted.");
+                return;
+            }
+            Authentication authToken = getAuthentication(claims, response);
             SecurityContextHolder.getContext().setAuthentication(authToken);
         }
         filterChain.doFilter(request,response);
     }
 
-    private Authentication getAuthentication(String token, HttpServletResponse response) throws IOException {
-        Claims claims = jwtUtil.parseToken(token);
+    private Authentication getAuthentication(Claims claims, HttpServletResponse response) throws IOException {
+
         UUID id = UUID.fromString(claims.getSubject());
         String role = claims.get("role").toString();
 
