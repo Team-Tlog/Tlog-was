b716fef241670285235a4e6537d89b9195fdd935
joongsu <112760801+joongsu@users.noreply.github.com>
2025-04-29 15:42:32 +0900

    Feature: Destination 필드 추가 및 수정 (#93)
    * feat: Destination 필드 추가 및 수정
    
    * feat: destination 필드에 review 점수 1~5점 반영 추가
    
    * feat: reviewCount 배열로 수정사항 반영

--- tlog-was/src/main/java/com/se/Tlog/domain/Travel/controller/dto/DestinationRes.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Travel/controller/dto/DestinationRes.java
@@ -4,33 +4,52 @@ import com.se.Tlog.domain.Travel.domain.Destination;
 import com.se.Tlog.domain.Travel.domain.Location;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public record DestinationRes(
         String id,
         String name,
         String address,
         Location location,
-        int rating,
         String city,
+        String district,
         boolean hasParking,
         boolean petFriendly,
-        List<TagIdDto> tags
+        int ratingSum,
+        int reviewCount,
+        float averageRating,
+        String imageUrl,
+        List<TagIdDto> tags,
+        Map<Integer,Integer> ratingDistribution
 ) {
     public static DestinationRes from(Destination destination) {
         List<TagIdDto> tagIdDtoList = destination.getTags().stream().filter(tagInfo -> !tagInfo.isDeleted())
                 .map(tagInfo -> TagIdDto.from(tagInfo.getId(), tagInfo.getWeight()))
                 .toList();
         System.out.println("destinationId = " + destination.getId());
+
+        Map<Integer, Integer> distribution = new TreeMap<>();
+        int[] ratingCount = destination.getRatingCount();
+        for (int i = 0; i < 5; i++) {
+            distribution.put(i+1, ratingCount[i]);
+        }
+
         return new DestinationRes(
                 destination.getId(),
                 destination.getName(),
                 destination.getAddress(),
                 destination.getLocation(),
-                destination.getRating(),
                 destination.getCity(),
+                destination.getDistrict(),
                 destination.isHasParking(),
                 destination.isPetFriendly(),
-                tagIdDtoList
+                destination.getRatingSum(),
+                destination.getReviewCount(),
+                destination.getAverageRating(),
+                destination.getImageUrl(),
+                tagIdDtoList,
+                distribution
         );
     }
 }
