c1a43d0e003f5335e44ddd3417f40f0083de2996
gojungsu <gjs4565@naver.com>
2025-04-30 19:16:03 +0900

    feat: 리뷰 생성시 자동 시간 추가를 위한 어노테이션 추가

--- tlog-was/src/main/java/com/se/Tlog/TlogApplication.java
+++ tlog-was/src/main/java/com/se/Tlog/TlogApplication.java
@@ -4,9 +4,11 @@ import com.se.Tlog.config.rabbitmq.RabbitMqChatProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RabbitMqChatProperties.class)
+@EnableMongoAuditing
 public class TlogApplication {
 
 	public static void main(String[] args) {
