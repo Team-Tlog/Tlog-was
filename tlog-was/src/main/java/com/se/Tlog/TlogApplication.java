0f006ea5a71a910a386655a17d8134d4b638130e
gojungsu <gjs4565@naver.com>
2025-05-01 17:21:40 +0900

    feat: 생성시간 자동 생성을 위한 어노테이션 추가

--- tlog-was/src/main/java/com/se/Tlog/TlogApplication.java
+++ tlog-was/src/main/java/com/se/Tlog/TlogApplication.java
@@ -4,11 +4,13 @@ import com.se.Tlog.config.rabbitmq.RabbitMqChatProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RabbitMqChatProperties.class)
 @EnableMongoAuditing
+@EnableJpaAuditing
 public class TlogApplication {
 
 	public static void main(String[] args) {
