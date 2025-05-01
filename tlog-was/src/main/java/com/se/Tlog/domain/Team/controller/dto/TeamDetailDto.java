ee15c120242024b3bd7a135f03c8e77b2677a9ab
gojungsu <gjs4565@naver.com>
2025-05-01 17:20:28 +0900

    feat: 프론트앤드 요구사항에 맞게 응답 포맷 수정 및 추가 작성

---
+++ tlog-was/src/main/java/com/se/Tlog/domain/Team/controller/dto/TeamDetailDto.java
@@ -0,0 +1,31 @@
+package com.se.Tlog.domain.Team.controller.dto;
+
+import com.se.Tlog.domain.Team.domain.Team;
+import com.se.Tlog.domain.Travel.controller.dto.SimpleDestinationRes;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public record TeamDetailDto(
+        UUID teamId,
+        String teamName,
+        //String teamTBTI,
+        long inviteCode,
+        LocalDate startDate,
+        LocalDate endDate,
+        List<TeamMemberDto> members,
+        List<SimpleDestinationRes> wishlist
+) {
+    public static TeamDetailDto from(Team team, List<TeamMemberDto> members, List<SimpleDestinationRes> wishlist) {
+        return new TeamDetailDto(
+                team.getId(),
+                team.getName(),
+                team.getInviteCode(),
+                team.getCreatedAt().toLocalDate(),
+                LocalDate.now(),
+                members,
+                wishlist
+        );
+    }
+}
