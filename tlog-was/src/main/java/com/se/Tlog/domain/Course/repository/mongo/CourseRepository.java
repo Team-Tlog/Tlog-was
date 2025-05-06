266f8198291cc0a03b0d1158c05147aa804aa3d1
patternplane <bss011202@naver.com>
2025-05-06 09:37:19 +0900

    feat: CourseRepository 작성

---
+++ tlog-was/src/main/java/com/se/Tlog/domain/Course/repository/mongo/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.se.Tlog.domain.Course.repository.mongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.se.Tlog.domain.Course.domain.Course;
+
+@Repository
+public interface CourseRepository extends MongoRepository<Course, String> {
+}
