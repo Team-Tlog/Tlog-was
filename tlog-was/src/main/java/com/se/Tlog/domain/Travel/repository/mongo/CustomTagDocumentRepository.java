d01ea6504231df8a24eaa9f1815e3e1ec34180e6
patternplane <bss011202@naver.com>
2025-05-08 22:59:07 +0900

    feat: 여행지-커스텀태그 N+1 문제 방지를 위한 일괄 조회 쿼리 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Travel/repository/mongo/CustomTagDocumentRepository.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Travel/repository/mongo/CustomTagDocumentRepository.java
@@ -4,9 +4,11 @@ import com.se.Tlog.domain.Travel.domain.CustomTagDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CustomTagDocumentRepository extends MongoRepository<CustomTagDocument,String> {
     Optional<CustomTagDocument> findByDestinationId(String destinationId);
+    List<CustomTagDocument> findAllByDestinationIdIn(List<String> destinationIds);
 }
