3f6d012a3033b1cd9d1233f573d5aa1c7192ba2f
patternplane <bss011202@naver.com>
2025-05-17 01:05:08 +0900

    feat: DestinationDomainService.isApproved() 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Travel/domain/service/DestinationDomainService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Travel/domain/service/DestinationDomainService.java
@@ -3,6 +3,8 @@ package com.se.Tlog.domain.Travel.domain.service;
 import com.se.Tlog.domain.Travel.repository.mongo.DestinationRepository;
 import com.se.Tlog.global.exception.CustomException;
 import com.se.Tlog.global.response.error.ErrorType;
+import com.se.Tlog.global.util.redis.RedisUtil;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DestinationDomainService {
     private final DestinationRepository destinationRepository;
+    private final RedisUtil redisUtil;
 
 
     public void validateExists(String destinationId) {
         if (!destinationRepository.existsById(destinationId)) {
             throw new CustomException(ErrorType.DESTINATION_NOT_FOUND);
         }
+    }
+
+    public boolean isApproved(String destinationId) {
+        return redisUtil.isInTaggingQueue(destinationId);
     }
 }
