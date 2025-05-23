30fa39be935f8ddd93a9592985ab26cd3bc8877b
patternplane <bss011202@naver.com>
2025-05-23 18:19:23 +0900

    refact: DestinationDomainService의 검수 상태 조회 메서드 변경

--- tlog-was/src/main/java/com/se/Tlog/domain/Travel/domain/service/DestinationDomainService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Travel/domain/service/DestinationDomainService.java
@@ -3,7 +3,6 @@ package com.se.Tlog.domain.Travel.domain.service;
 import com.se.Tlog.domain.Travel.repository.mongo.DestinationRepository;
 import com.se.Tlog.global.exception.CustomException;
 import com.se.Tlog.global.response.error.ErrorType;
-import com.se.Tlog.global.util.redis.RedisUtil;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DestinationDomainService {
     private final DestinationRepository destinationRepository;
-    private final RedisUtil redisUtil;
 
 
     public void validateExists(String destinationId) {
@@ -22,6 +20,6 @@ public class DestinationDomainService {
     }
 
     public boolean isApproved(String destinationId) {
-        return redisUtil.isInTaggingQueue(destinationId);
+        return destinationRepository.existsById(destinationId);
     }
 }
