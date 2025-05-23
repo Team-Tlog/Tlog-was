1665554df14854a1938b5de6922d5452f0b10f34
patternplane <bss011202@naver.com>
2025-05-23 18:17:57 +0900

    delete: RedisUtil 미검수 여행지 관리기능 삭제

--- tlog-was/src/main/java/com/se/Tlog/global/util/redis/RedisProperties.java
+++ tlog-was/src/main/java/com/se/Tlog/global/util/redis/RedisProperties.java
@@ -3,5 +3,10 @@ package com.se.Tlog.global.util.redis;
 public interface RedisProperties {
     String REFRESH_TOKEN_PREFIX = "refresh_token:";
     String ACCESS_TOKEN_PREFIX = "access_token:";
-    String PENDING_TAGGING_DESTINATION = "pending:tagging:destination";
+    /*
+     * 25.05.22
+     * 태그 처리되지 않고 검수되지 않은 여행지는
+     * Redis로 기록되지 않고 별도의 DB공간에 기록되도록 변경되었습니다. 
+     * String PENDING_TAGGING_DESTINATION = "pending:tagging:destination";
+     */ 
 }
