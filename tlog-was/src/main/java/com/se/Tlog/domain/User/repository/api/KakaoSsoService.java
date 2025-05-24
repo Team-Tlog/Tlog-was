fb5ecf7c1037baf5b9a455d64d4129ec1a170ba1
patternplane <bss011202@naver.com>
2025-05-25 00:18:27 +0900

    fix: 소셜로그인 성공 응답으로부터 이름이 적절히 조회되지 않는 문제 수정

--- tlog-was/src/main/java/com/se/Tlog/domain/User/repository/api/KakaoSsoService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/User/repository/api/KakaoSsoService.java
@@ -31,11 +31,19 @@ public class KakaoSsoService implements SsoService {
 
                 System.out.println("🔹 [DEBUG] Kakao API Response: " + response.getBody());
 
-                JsonNode jsonNode = objectMapper.readTree(response.getBody());
-                // 전화번호 정보 가져오기 (사용자가 동의했을 경우에만 존재)
-                // String phoneNumber = jsonNode.path("kakao_account").path("phone_number").asText(null);
-
-                return SsoUserInfo.from(jsonNode,"kakao");
+                JsonNode rootResponseNode = objectMapper.readTree(response.getBody());
+                JsonNode profileNode = rootResponseNode.at("/kakao_account/profile");
+                
+                // 이메일 및 전화번호 등 기타 정보는 프론트 앱이 비즈 앱으로 등록되어야 조회할 수 있습니다.
+                //    자세한 사항은 공식문서 및 카카오 동의항목 설정 페이지 참고 (Kakao Developers)
+                //    https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info-response
+                //    https://developers.kakao.com/console/app/1197999/product/login/scope
+                return new SsoUserInfo(
+                        rootResponseNode.path("id").asText(),
+                        "", // profileNode.path("email").asText(""), // 이메일
+                            // profileNode.path("phone_number").asText(""), // 전화번호
+                        profileNode.path("nickname").asText(""),
+                        "kakao");
             } catch (RestClientResponseException e){
                 System.err.println("❌ [ERROR] Kakao API 호출 실패: " + e.getStatusCode() + " / " + e.getResponseBodyAsString());
                 throw new CustomException(ErrorType.KAKAO_AUTH_FAIL);
