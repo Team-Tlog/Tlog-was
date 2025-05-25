f05a3aa3ced48292078fb0513983b0fc380d8dbf
patternplane <bss011202@naver.com>
2025-05-26 07:51:02 +0900

    feat: 검색 기능 확장에 따른 SearchService 변경

--- tlog-was/src/main/java/com/se/Tlog/domain/Search/application/SearchService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Search/application/SearchService.java
@@ -2,9 +2,13 @@ package com.se.Tlog.domain.Search.application;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.se.Tlog.domain.ApplicationService;
-import com.se.Tlog.domain.Search.repository.DestinationRawData;
 import com.se.Tlog.domain.Search.repository.mongo.DestinationSearchRepository;
+import com.se.Tlog.domain.Travel.application.DestinationService;
+import com.se.Tlog.domain.Travel.controller.dto.DestinationSummaryRes;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,8 +16,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SearchService {
 	private final DestinationSearchRepository searchRepository;
+	private final DestinationService destinationService;
+    
+    public List<DestinationSummaryRes> autoCompleteDestinationByAddress(String address) {
+        return destinationService.convertToDto(
+                searchRepository.autoCompleteByAddress(address));
+    }
 	
-	public List<DestinationRawData> autoComplete(String searchText) {
-		return searchRepository.autoCompleteBy(searchText);
+	public List<DestinationSummaryRes> autoCompleteDestinationByName(String name) {
+		return destinationService.convertToDto(
+		        searchRepository.autoCompleteByName(name));
 	}
+	
+	public Page<DestinationSummaryRes> searchDestinationByCity(Pageable pageable, String city) {
+	    return destinationService.convertToDto(
+	            searchRepository.searchByCity(city, pageable));
+    }
 }
