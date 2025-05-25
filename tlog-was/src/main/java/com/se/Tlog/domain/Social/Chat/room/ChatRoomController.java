2c7425e3a65c42731b35ad4b8f0fb9b28297291f
gojungsu <gjs4565@naver.com>
2025-05-25 16:41:05 +0900

    feat : 팀 생성 시 팀 채팅방 자동 생성 기능 작성 및 팀 참가시 채팅방 자동 참여 기능 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/room/ChatRoomController.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/room/ChatRoomController.java
@@ -1,14 +1,11 @@
 package com.se.Tlog.domain.Social.Chat.room;
 
-import com.se.Tlog.domain.Social.Chat.room.dto.RequestInviteList;
-import com.se.Tlog.domain.Social.Chat.room.dto.ChatRoomResponseDto;
 import com.se.Tlog.domain.User.domain.Role;
 import com.se.Tlog.domain.User.domain.User;
 import com.se.Tlog.domain.User.repository.jpa.UserRepository;
 import com.se.Tlog.global.exception.CustomException;
 import com.se.Tlog.global.response.error.ErrorType;
 import com.se.Tlog.global.response.success.SuccessRes;
-import com.se.Tlog.global.response.success.SuccessType;
 import com.se.Tlog.global.util.jwt.AccessTokenProvider;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -28,26 +25,6 @@ public class ChatRoomController {
     private final ChatRoomService chatRoomService;
     private final AccessTokenProvider accessTokenProvider;
     private final UserRepository userRepository;    // 테스트 때문에 넣어둠 나중에 지우겠습니다!
-    @PostMapping("/room/{hostId}")
-    @Operation(
-            summary = "채팅방을 생성합니다.",
-            description = "특정 유저가 참여하고 있는 채팅방 리스트 조회합니다.",
-            tags = {"ChatRoom 관리"},
-            security = @SecurityRequirement(
-                    name = "JwtAuthScheme",
-                    scopes = {"scope1", "scope2"}),
-            parameters = {@Parameter(name = "hostId", description = "채팅방 리스트 조회하는 유저의 UUID 입니다.")},
-            responses = {
-                    @ApiResponse( responseCode = "200", description = "성공"),
-                    @ApiResponse( responseCode = "400", description = "초대하려는 유저 중 존재하지 않는 유저가 있습니다."),
-                    @ApiResponse(responseCode = "500", description = "서버 내부 오류")
-            }
-    )
-    public ResponseEntity<?> createRoom(@PathVariable(name = "hostId") UUID hostId,
-                                        @RequestBody RequestInviteList requestInviteList) {
-        ChatRoomResponseDto chatRoomResponseDto = chatRoomService.create(hostId, requestInviteList);
-        return ResponseEntity.ok().body(SuccessRes.of(SuccessType.CHATROOM_CREATE,chatRoomResponseDto));
-    }
 
     @GetMapping("/room/{hostId}")
     @Operation(
--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/room/repository/jpa/ChatRoomRepository.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/room/repository/jpa/ChatRoomRepository.java
@@ -4,6 +4,10 @@ import com.se.Tlog.domain.Social.Chat.room.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public interface ChatRoomRepository extends JpaRepository<ChatRoom,Long> {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findByTeamId(UUID teamId);
 }
