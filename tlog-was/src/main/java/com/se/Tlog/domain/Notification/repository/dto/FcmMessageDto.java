17bfe0d96be02a24a0504aa2783dbbb97bf128a8
patternplane <bss011202@naver.com>
2025-05-27 10:49:27 +0900

    feat: Message DTO에 null값 검증 로직 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Notification/repository/dto/FcmMessageDto.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Notification/repository/dto/FcmMessageDto.java
@@ -2,8 +2,20 @@ package com.se.Tlog.domain.Notification.repository.dto;
 
 import java.util.Map;
 
-public record FcmMessageDto(
-		String fcmToken,
-		Map<String, String> payload) {
+import com.se.Tlog.global.exception.CustomException;
+import com.se.Tlog.global.response.error.ErrorType;
 
+import lombok.Getter;
+
+@Getter
+public class FcmMessageDto {
+    private String fcmToken;
+    private Map<String, String> payload;
+    
+    public FcmMessageDto(String fcmToken, Map<String, String> payload) {
+        if (fcmToken == null || payload.values().stream().anyMatch(v -> v == null))
+            throw new CustomException(ErrorType.NULL_IN_FCM_MESSAGE);
+        this.fcmToken = fcmToken;
+        this.payload = payload;
+    }
 }
--- tlog-was/src/main/java/com/se/Tlog/domain/Notification/repository/dto/FcmRequestDto.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Notification/repository/dto/FcmRequestDto.java
@@ -3,8 +3,20 @@ package com.se.Tlog.domain.Notification.repository.dto;
 import java.util.Map;
 import java.util.UUID;
 
-public record FcmRequestDto(
-		UUID userId,
-		Map<String, String> payload) {
+import com.se.Tlog.global.exception.CustomException;
+import com.se.Tlog.global.response.error.ErrorType;
 
+import lombok.Getter;
+
+@Getter
+public class FcmRequestDto {
+    private UUID userId;
+    private Map<String, String> payload;
+    
+    public FcmRequestDto(UUID userId, Map<String, String> payload) {
+        if (userId == null || payload.values().stream().anyMatch(v -> v == null))
+            throw new CustomException(ErrorType.NULL_IN_FCM_MESSAGE);
+        this.userId = userId;
+        this.payload = payload;
+    }
 }
