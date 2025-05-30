1a566daee7980e4acdc8c7d94815a787dbdc7dde
patternplane <bss011202@naver.com>
2025-05-30 16:04:27 +0900

    feat: 팀 관련 DTO 내 초대코드 형태를 6자리 문자열로 변경

--- tlog-was/src/main/java/com/se/Tlog/domain/Team/controller/dto/TeamUserRequestDto.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Team/controller/dto/TeamUserRequestDto.java
@@ -6,8 +6,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "기본적인 팀원 관리 요청에 사용되는 DTO. 팀 초대, 팀원으로 추가, 팀 방출에 사용됩니다.")
 public record TeamUserRequestDto(
-		@Schema(description = "처리할 팀의 code")
-		long inviteCode,
+		@Schema(description = "처리할 팀의 초대 코드")
+		String inviteCode,
 		
 		@Schema(description = "처리할 유저의 id")
 		UUID userId
