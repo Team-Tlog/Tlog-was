ac0ba59e136f5cdee4da6460e5d979c475e982e0
gojungsu <gjs4565@naver.com>
2025-06-01 21:33:10 +0900

    feat : 동시성 이슈 반영하여 DestinationDomainService 리뷰 추가시 필드 카운트 업데이트 기능 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Travel/domain/service/DestinationDomainService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Travel/domain/service/DestinationDomainService.java
@@ -1,17 +1,24 @@
 package com.se.Tlog.domain.Travel.domain.service;
 
+import com.mongodb.client.result.UpdateResult;
+import com.se.Tlog.domain.Travel.domain.Destination;
 import com.se.Tlog.domain.Travel.repository.mongo.DestinationRepository;
 import com.se.Tlog.global.exception.CustomException;
 import com.se.Tlog.global.response.error.ErrorType;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class DestinationDomainService {
     private final DestinationRepository destinationRepository;
-
+    private final MongoTemplate mongoTemplate;
 
     public void validateExists(String destinationId) {
         if (!destinationRepository.existsById(destinationId)) {
@@ -19,6 +26,38 @@ public class DestinationDomainService {
         }
     }
 
+    public void increaseReviewCountAndRating(String destinationId, int rating) {
+        Destination destination = mongoTemplate.findById(destinationId, Destination.class);
+        if (destination == null) {
+            throw new CustomException(ErrorType.DESTINATION_NOT_FOUND);
+        }
+        float approximateAverage = (float) (destination.getRatingSum() + rating) / (destination.getReviewCount());
+
+        Update update = new Update()
+                .inc("reviewCount", 1)
+                .inc("ratingSum", rating)
+                .inc("ratingCount." + (rating - 1), 1)
+                .set("averageRating", approximateAverage);
+
+        UpdateResult updateResult = mongoTemplate.updateFirst(
+                Query.query(Criteria.where("_id").is(destinationId)),
+                update,
+                Destination.class
+        );
+
+        if (updateResult.getMatchedCount() == 0) {
+            throw new CustomException(ErrorType.DESTINATION_NOT_FOUND);
+        }
+
+    }
+
+    public void decreaseReviewCount(String destinationId) {
+        mongoTemplate.updateFirst(
+                Query.query(Criteria.where("_id").is(destinationId)),
+                new Update().inc("reviewCount", -1),
+                Destination.class
+        );
+    }
     public boolean isApproved(String destinationId) {
         return destinationRepository.existsById(destinationId);
     }
