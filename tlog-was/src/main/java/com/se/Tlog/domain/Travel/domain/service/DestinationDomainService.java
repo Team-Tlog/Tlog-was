aa9f1a39a55d913a53addca2e948b135108dbb9e
patternplane <bss011202@naver.com>
2025-06-04 22:15:33 +0900

    feat: 평점 분포 반환 DestinationDomainService.getRatingDistribution() 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Travel/domain/service/DestinationDomainService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Travel/domain/service/DestinationDomainService.java
@@ -8,6 +8,10 @@ import com.se.Tlog.global.exception.CustomException;
 import com.se.Tlog.global.response.error.ErrorType;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +23,17 @@ public class DestinationDomainService {
     private final MongoTemplate mongoTemplate;
     private final DestinationRepositoryService destinationRepositoryService;
 
+    public Map<Integer, Integer> getRatingDistribution(String destinationId) {
+        int[] distribution = destinationRepository.findById(destinationId)
+                .orElseThrow(() -> new CustomException(ErrorType.DESTINATION_NOT_FOUND))
+                .getRatingCount();
+        
+        Map<Integer, Integer> resultMap = new TreeMap<>();
+        for (int i = 0; i < 5; i++)
+            resultMap.put(i+1, distribution[i]);
+        return resultMap;
+    }
+    
     public void validateExists(String destinationId) {
         if (!destinationRepository.existsById(destinationId)) {
             throw new CustomException(ErrorType.DESTINATION_NOT_FOUND);
