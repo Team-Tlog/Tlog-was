76cf2cab19fa332c9c6a6ec1a033c403f7daddde
patternplane <bss011202@naver.com>
2025-06-04 23:36:06 +0900

    feat: UserRewardDto에 isDefaultReward 정보 추가

--- tlog-was/src/main/java/com/se/Tlog/domain/Reward/controller/dto/UserRewardDto.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Reward/controller/dto/UserRewardDto.java
@@ -7,6 +7,7 @@ public record UserRewardDto(
         Long rewardId, // 서비스의 고유 보상 id인 RewardInfo의 id입니다!
         String name,
         String description,
-        String iconImageUrl) {
+        String iconImageUrl,
+        boolean isDefaultReward) {
 
 }
