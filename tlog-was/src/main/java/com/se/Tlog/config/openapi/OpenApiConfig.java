7cf46650812e7e64cc1321d2c1c7189d3b476d81
patternplane <bss011202@naver.com>
2025-06-07 21:41:30 +0900

    docs: Swagger UI 서버 설정 추가 (#180)

--- tlog-was/src/main/java/com/se/Tlog/config/openapi/OpenApiConfig.java
+++ tlog-was/src/main/java/com/se/Tlog/config/openapi/OpenApiConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition( // Swagger UI 페이지 설정
         info = @Info(
@@ -15,7 +16,10 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
                 description = "Tlog 백엔드 서버의 API 명세를 기록한 문서입니다."
                 // license = @License(name = "Apache 2.0", url = "http://foo.bar"),
                 // contact = @Contact(url = "http://myserver.com", name = "name", email = "name@email.com")
-        )
+        ),
+        servers = {
+                @Server(url = "/", description = "서버 선택")
+        }
 )
 @SecurityScheme ( // Swagger UI 페이지 내 인증 기능 설정
 		name = "JwtAuthScheme",
