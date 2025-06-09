ab4ce83ccf4a1d14195a7a18ce8284a69e65812e
gojungsu <gjs4565@naver.com>
2025-06-10 01:31:04 +0900

    fix : publish 할 때 ChatMessage 엔티티 내부 필드 Lazy로딩 때문에 직렬화 문제 발생

--- tlog-was/src/main/java/com/se/Tlog/global/security/handler/StompHandler.java
+++ tlog-was/src/main/java/com/se/Tlog/global/security/handler/StompHandler.java
@@ -11,8 +11,6 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
 import java.security.Principal;
-import java.util.List;
-import java.util.Map;
 
 
 @Slf4j
@@ -28,12 +26,6 @@ public class StompHandler implements ChannelInterceptor {
         System.out.println("accessor = " + accessor);
         Principal principal = (Principal) accessor.getSessionAttributes().get("userPrincipal");
         accessor.setUser(principal);
-
-        Map<String, List<String>> nativeHeaderMap = accessor.toNativeHeaderMap();
-        nativeHeaderMap.forEach((key,values) ->{
-            System.out.println("헤더 key: " + key + ", value(s): " + values);
-        });
-
         switch (accessor.getCommand()) {
             case CONNECT -> chatSessionEventService.handleConnect(accessor); //STOMP 세션 연결 성공
             case SUBSCRIBE -> chatSessionEventService.handleSubscribe(accessor); //채팅 구독 요청
@@ -47,23 +39,14 @@ public class StompHandler implements ChannelInterceptor {
     public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
-        Map<String, List<String>> nativeHeaderMap = accessor.toNativeHeaderMap();
-        System.out.println("PostSend Accessor");
-        nativeHeaderMap.forEach((key,values) ->{
-            System.out.println("헤더 key: " + key + ", value(s): " + values);
-        });
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-            Principal user = (Principal) accessor.getSessionAttributes().get("userPrincipal");
-            System.out.println("user = " + user);
+            Principal user = accessor.getUser();
             if (user instanceof CustomPrincipal customPrincipal) {
                 String nickname = customPrincipal.nickname();
-                accessor.setNativeHeader("user-name", nickname != null && !nickname.isBlank() ? nickname : "unknown");
+                accessor.addNativeHeader("user-name", nickname != null ? nickname : "null");
             } else {
-                accessor.setNativeHeader("user-name", "unknown");
+                accessor.addNativeHeader("user-name", "null");
             }
         }
-
-        String usernameHeader = accessor.getFirstNativeHeader("user-name");
-        log.info("user-name header: {}", usernameHeader);
     }
 }
