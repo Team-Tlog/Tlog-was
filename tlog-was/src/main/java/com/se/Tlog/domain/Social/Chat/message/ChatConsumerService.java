0a2c8b6cc630a91b428b9d62dbcd406cfe32c2a2
gojungsu <gjs4565@naver.com>
2025-06-10 20:59:44 +0900

    feat : 기존 consumer 로직에 채팅방 목록 갱신을 위한 로직 추가

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/message/ChatConsumerService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/message/ChatConsumerService.java
@@ -2,9 +2,15 @@ package com.se.Tlog.domain.Social.Chat.message;
 
 import com.se.Tlog.domain.Social.Chat.message.dto.ChatMessageDto;
 import com.se.Tlog.domain.Social.Chat.message.dto.ChatMessageRequestDto;
+import com.se.Tlog.domain.Social.Chat.message.dto.TeamChatSummaryDto;
+import com.se.Tlog.domain.Social.Chat.read.ChatReadUsers;
+import com.se.Tlog.domain.Social.Chat.read.repository.jpa.ChatReadStatusRepository;
+import com.se.Tlog.domain.Social.Chat.read.repository.jpa.ChatReadUsersRepository;
 import com.se.Tlog.domain.Social.Chat.room.ChatRoom;
 import com.se.Tlog.domain.Social.Chat.message.repository.jpa.ChatMessageRepository;
 import com.se.Tlog.domain.Social.Chat.room.repository.jpa.ChatRoomRepository;
+import com.se.Tlog.domain.Social.Chat.room.repository.jpa.ChatRoomUserRepository;
+import com.se.Tlog.domain.Social.Chat.session.repository.jpa.ConnectedUserSessionRepository;
 import com.se.Tlog.domain.User.domain.User;
 import com.se.Tlog.domain.User.repository.jpa.UserRepository;
 import com.se.Tlog.global.exception.CustomException;
@@ -14,6 +20,8 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.*;
+
 @Service
 @RequiredArgsConstructor
 public class ChatConsumerService {
@@ -21,6 +29,11 @@ public class ChatConsumerService {
     private final ChatMessageRepository chatMessageRepository;
     private final SimpMessageSendingOperations messagingTemplate;
     private final UserRepository userRepository;
+    private final ChatRoomUserRepository chatRoomUserRepository;
+    private final ChatReadUsersRepository chatReadUsersRepository;
+    private final ChatReadStatusRepository chatReadStatusRepository;
+    private final ConnectedUserSessionRepository connectedUserSessionRepository;
+
 
     @Transactional
     public void receivedChatMessage(ChatMessageRequestDto chatMessageRequestDto) {
@@ -34,9 +47,45 @@ public class ChatConsumerService {
 
         chatRoom.modifyLastMessage(chatMessage.getId());
 
+        ChatReadUsers chatReadUsers = ChatReadUsers.create(sender, chatMessage);
+        chatReadUsersRepository.save(chatReadUsers);
+
         messagingTemplate.convertAndSend(
                 "/sub/chat/room/" + chatRoom.getId(),
                 ChatMessageDto.from(sender, chatRoom, savedChatMessage)
         );
+
+        // connectedUserIds = 현재 채팅방 구독중인 유저
+        Set<UUID> connectedUserIds = connectedUserSessionRepository.findConnectedUserIdsByRoomId(chatRoom.getId());
+        List<User> participants = chatRoomUserRepository.findParticipantsByChatRoomId(chatRoom.getId());
+
+        // 모두가 구독중인 경우 실시간 채팅 목록 갱신해줄 필요 x
+        boolean allConnected = participants.stream()
+                .allMatch(user -> connectedUserIds.contains(user.getId()));
+        if (allConnected) {
+            return;
+        }
+
+        List<UUID> targetUserIds = participants.stream()
+                .map(User::getId)
+                .filter(userId -> !connectedUserIds.contains(userId))
+                .toList();
+        List<Object[]> readStatuses = chatReadStatusRepository.findLastReadMessageIdsForParticipants(targetUserIds, chatRoom.getId());
+        Map<UUID, Long> lastReadMap = new HashMap<>();
+
+        for (Object[] row : readStatuses) {
+            UUID userId = (UUID) row[0];
+            Long lastReadMessageId = (Long) row[1];
+            lastReadMap.put(userId, lastReadMessageId);
+        }
+
+        for (UUID targetUserId : targetUserIds) {
+            Long lastReadMessage = lastReadMap.getOrDefault(targetUserId, 0L);
+            int unreadCount = chatMessageRepository.countUnreadMessages(chatRoom.getId(), lastReadMessage);
+            messagingTemplate.convertAndSend(
+                    "/sub/chat/room" + targetUserId,
+                    TeamChatSummaryDto.from(targetUserId, chatRoom.getId(), savedChatMessage, unreadCount)
+            );
+        }
     }
 }
