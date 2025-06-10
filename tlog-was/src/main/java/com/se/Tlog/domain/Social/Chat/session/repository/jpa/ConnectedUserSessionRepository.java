9998b69d15495b0a88995fd209aa033ed9f0c188
gojungsu <gjs4565@naver.com>
2025-06-10 20:53:51 +0900

    feat : 채팅방을 구독중인 세션의 userId 반환

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/session/repository/jpa/ConnectedUserSessionRepository.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/session/repository/jpa/ConnectedUserSessionRepository.java
@@ -2,8 +2,15 @@ package com.se.Tlog.domain.Social.Chat.session.repository.jpa;
 
 import com.se.Tlog.domain.Social.Chat.session.ConnectedUserSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Repository
 public interface ConnectedUserSessionRepository extends JpaRepository<ConnectedUserSession, String> {
+    @Query("SELECT cus.userId from ConnectedUserSession cus where cus.roomId = :roomId")
+    Set<UUID> findConnectedUserIdsByRoomId(@Param("roomId") Long roomId);
 }
