97cb40633dfefdbf598dfb6c50e70637f7cf4f92
gojungsu <gjs4565@naver.com>
2025-06-10 20:59:11 +0900

    feat : 채팅방에 가입한 유저 조회

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/room/repository/jpa/ChatRoomUserRepository.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/room/repository/jpa/ChatRoomUserRepository.java
@@ -2,6 +2,7 @@ package com.se.Tlog.domain.Social.Chat.room.repository.jpa;
 
 
 import com.se.Tlog.domain.Social.Chat.room.ChatRoomUser;
+import com.se.Tlog.domain.User.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,11 +11,14 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 @Repository
-public interface ChatRoomUserRepository extends JpaRepository<ChatRoomUser, UUID> {
+public interface ChatRoomUserRepository extends JpaRepository<ChatRoomUser, Long> {
 
     @Query("SELECT cru from ChatRoomUser cru join fetch cru.chatRoom where cru.user.id = :userId")
     List<ChatRoomUser> findChatRoomByUserId(@Param("userId") UUID hostId);
 
     @Query("select count(cru) from ChatRoomUser cru where cru.chatRoom.id = :roomId")
     int countChatRoomJoinUsers(@Param("roomId") Long roomId);
+
+    @Query("SELECT cru.user FROM ChatRoomUser cru WHERE cru.chatRoom.id = :chatRoomId")
+    List<User> findParticipantsByChatRoomId(@Param("chatRoomId") Long roomId);
 }
