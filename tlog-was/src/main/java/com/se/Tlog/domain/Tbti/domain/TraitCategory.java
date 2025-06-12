949dfab875977dffe091caf7108d5edac486a619
Baek SeongSoo <bss011202@naver.com>
2025-06-12 15:38:03 +0900

    Feature: TBTI 카테고리 산정 정책 변경 (#212)
    * feat: TBTI TraitCategory 비율 산정 정책 변경에 따른 이니셜 수정
    
    * test: TBTI 산정 정책 변경에 따른 TbtiTest 수정

--- tlog-was/src/main/java/com/se/Tlog/domain/Tbti/domain/TraitCategory.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Tbti/domain/TraitCategory.java
@@ -4,9 +4,9 @@ import com.se.Tlog.global.exception.CustomException;
 import com.se.Tlog.global.response.error.ErrorType;
 
 public enum TraitCategory {
-    RISK_TAKING("S-R"), // 교류형(S) ↔ 사색형(R) 0 ~ 99
+    RISK_TAKING("R-S"), // 사색형(R) ↔ 교류형(S) 0 ~ 99
     LOCATION_PREFERENCE("E-O"), // 자연친화(E) ↔ 도시친화(O) 0 ~ 99
-    PLANNING_STYLE("L-N"), // 계획적(L) ↔ 즉흥적(N) 0 ~ 99
+    PLANNING_STYLE("N-L"), // 즉흥적(N) ↔ 계획적(L) 0 ~ 99
     ACTIVITY_LEVEL("A-I"); // 활동형(A) ↔ 여유형(I) 0 ~ 99
 
     private String categoryInitial;
