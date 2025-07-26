fcfb9bc892eace23a1b994aee2dde911eb927e79
patternplane <bss011202@naver.com>
2025-07-26 15:13:18 +0900

    feat: 추천 게시글 조회를 위한 PostRepositoryExtension내 쿼리 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Post/repository/mongo/PostRepositoryExtension.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Post/repository/mongo/PostRepositoryExtension.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Component;
 
 import com.se.Tlog.domain.Social.Post.domain.Post;
+import com.se.Tlog.domain.User.repository.jpa.UserRepositoryExtension;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,18 +19,37 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PostRepositoryExtension {
     private final PostRepository postRepository;
+    private final UserRepositoryExtension userRepositoryExtension;
     
     private static final String NULL_LAST_POST_ID = "ffffffffffffffffffffffff";
+    
+    private Slice<Post> queryPostsFromUserIds(int size, String lastPostId, List<UUID> userIds) {
+        if (0 == userIds.size())
+            return new SliceImpl<Post>(new ArrayList<>(), PageRequest.ofSize(0), false);
+        
+        List<Post> results = new ArrayList<Post>(
+                postRepository.findAllRecentPostsByUsers(size + 1, lastPostId, userIds));
+        boolean hasNext = (size + 1 == results.size());
+        if (hasNext) results.remove(size);
+        return new SliceImpl<Post>(results, PageRequest.ofSize(size), hasNext);
+    }
     
     public Slice<Post> findAllFollowersRecentPosts(int size, String lastPostId, List<UUID> followingList) {
         if (size <= 0) size = 10;
         if (lastPostId == null || !ObjectId.isValid(lastPostId)) lastPostId = NULL_LAST_POST_ID;
         if (followingList == null) followingList = new ArrayList<UUID>();
+
+        return queryPostsFromUserIds(size, lastPostId, followingList);
+    }
+    
+    public Slice<Post> findAllRecentSuggestedPosts(int size, String lastPostId, UUID userId) {
+        if (size <= 0) size = 10;
+        if (lastPostId == null || !ObjectId.isValid(lastPostId)) lastPostId = NULL_LAST_POST_ID;
         
-        List<Post> results = new ArrayList<Post>(
-                postRepository.findAllRecentPostsByUsers(size + 1, lastPostId, followingList));
-        boolean hasNext = (size + 1 == results.size());
-        if (hasNext) results.remove(size);
-        return new SliceImpl<Post>(results, PageRequest.ofSize(size), hasNext);
+        // 유사한 TBTI를 가진 모든 유저를 조회 -> 해당 조건으로 게시글 필터링
+        // 비효율적이며 성능에 문제가 우려됨 : 추후 게시글 자체에 TBTI 정보를 포함하는 등의 개선안 고려 필요
+        
+        List<UUID> relativeUsers = userRepositoryExtension.findAllIdsByRelativeTbti(userId);
+        return queryPostsFromUserIds(size, lastPostId, relativeUsers);
     }
 }
