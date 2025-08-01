4fd148600a89400e65d62f9521cad5656fad717f
patternplane <bss011202@naver.com>
2025-08-01 22:49:58 +0900

    feat: OperationService 게시글 신고 기능 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Admin/application/OperationService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Admin/application/OperationService.java
@@ -1,9 +1,13 @@
 package com.se.Tlog.domain.Admin.application;
 
 import com.se.Tlog.domain.Admin.controller.dto.CreateFeedbackReq;
+import com.se.Tlog.domain.Admin.controller.dto.CreateReportToPostReq;
 import com.se.Tlog.domain.Admin.domain.Feedback;
+import com.se.Tlog.domain.Admin.domain.ReportOfPost;
 import com.se.Tlog.domain.Admin.repository.mongo.FeedbackRepository;
+import com.se.Tlog.domain.Admin.repository.mongo.ReportOfPostRepository;
 import com.se.Tlog.domain.ApplicationService;
+import com.se.Tlog.domain.Social.Post.repository.mongo.PostRepository;
 import com.se.Tlog.global.exception.CustomException;
 import com.se.Tlog.global.response.error.ErrorType;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +18,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OperationService {
     private final FeedbackRepository feedbackRepository;
+    private final PostRepository postRepository;
+    private final ReportOfPostRepository reportOfPostRepository;
 
     public void makeFeedback(UUID userId, CreateFeedbackReq request) {
         if (userId == null)
@@ -27,5 +33,19 @@ public class OperationService {
                 request.content(),
                 request.refImageUrls());
         feedbackRepository.save(newFeedback);
+    }
+
+    public void makeReportToPost(UUID reporterId, CreateReportToPostReq request) {
+        if (reporterId == null)
+            throw new CustomException(ErrorType.UN_AUTHORIZATION);
+        if (request == null || request.postId() == null)
+            throw new CustomException(ErrorType.ILLEGAL_ARGUMENT);
+        if (!postRepository.existsById(request.postId()))
+            throw new CustomException(ErrorType.POST_NOT_FOUND);
+
+        ReportOfPost report = reportOfPostRepository.findByPostId(request.postId());
+        if (report != null) report.addReport(reporterId);
+        else report = ReportOfPost.create(request.postId(), reporterId);
+        reportOfPostRepository.save(report);
     }
 }
