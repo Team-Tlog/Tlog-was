5e92ff2299b0829fbcac3b4c8bb62c8d25f998a0
patternplane <bss011202@naver.com>
2025-08-01 10:36:08 +0900

    feat: TbtiInfoRes에서 imageUrl 제거, 선호/비선호 유형 추가

--- tlog-was/src/main/java/com/se/Tlog/domain/Tbti/controller/dto/TbtiInfoRes.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Tbti/controller/dto/TbtiInfoRes.java
@@ -7,16 +7,18 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "TBTI 특성에 관한 정보를 표시하는 데이터입니다.")
 public record TbtiInfoRes(
         String tbtiString,
-        String imageUrl,
         String secondName,
-        String description) {
+        String description,
+        String preferredTbti,
+        String notPreferredTbti) {
 
     public static TbtiInfoRes getNullDto(Tbti tbti) {
         String tbtiStr = tbti.toString();
         return new TbtiInfoRes(
-                tbtiStr, 
-                "", 
-                "아직 " + tbtiStr + "의 정보가 없어요...", 
-                "아직 " + tbtiStr + "가 어떤 성향인지 조사하고 있어요!");
+                tbtiStr,
+                "아직 " + tbtiStr + "의 정보가 없어요...",
+                "아직 " + tbtiStr + "가 어떤 성향인지 조사하고 있어요!",
+                tbtiStr,
+                tbtiStr);
     }
 }
