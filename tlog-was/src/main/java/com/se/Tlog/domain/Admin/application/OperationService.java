bb13e32e2442ce6dccb256f0de77cd164552a2b0
patternplane <bss011202@naver.com>
2025-08-01 21:57:03 +0900

    fix: OperationService에서 유효성 검증을 하지 않는 문제 수정

--- tlog-was/src/main/java/com/se/Tlog/domain/Admin/application/OperationService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Admin/application/OperationService.java
@@ -4,6 +4,8 @@ import com.se.Tlog.domain.Admin.controller.dto.CreateFeedbackReq;
 import com.se.Tlog.domain.Admin.domain.Feedback;
 import com.se.Tlog.domain.Admin.repository.mongo.FeedbackRepository;
 import com.se.Tlog.domain.ApplicationService;
+import com.se.Tlog.global.exception.CustomException;
+import com.se.Tlog.global.response.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
@@ -14,6 +16,11 @@ public class OperationService {
     private final FeedbackRepository feedbackRepository;
 
     public void makeFeedback(UUID userId, CreateFeedbackReq request) {
+        if (userId == null)
+            throw new CustomException(ErrorType.UN_AUTHORIZATION);
+        if (request == null)
+            throw new CustomException(ErrorType.ILLEGAL_ARGUMENT);
+
         Feedback newFeedback = Feedback.create(
                 userId,
                 request.title(),
