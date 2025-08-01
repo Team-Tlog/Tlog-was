cdfc4bfd2b52b7ead7703775bb82941e57f5887c
patternplane <bss011202@naver.com>
2025-08-01 10:38:10 +0900

    docs: TbtiInfoRes DTO의 Example 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/Tbti/controller/dto/TbtiInfoRes.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Tbti/controller/dto/TbtiInfoRes.java
@@ -6,10 +6,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "TBTI 특성에 관한 정보를 표시하는 데이터입니다.")
 public record TbtiInfoRes(
+        @Schema(description = "TBTI 유형입니다.", example = "RENA")
         String tbtiString,
+        @Schema(description = "TBTI 유형의 다른 이름입니다.", example = "자연 속 즉흥 사색가")
         String secondName,
+        @Schema(description = "TBTI 설명입니다.", example = "자연의 향기와 소리를 ~")
         String description,
+        @Schema(description = "어울리는 TBTI 유형입니다.", example = "RENA")
         String preferredTbti,
+        @Schema(description = "상극인 TBTI 유형입니다.", example = "RENA")
         String notPreferredTbti) {
 
     public static TbtiInfoRes getNullDto(Tbti tbti) {
