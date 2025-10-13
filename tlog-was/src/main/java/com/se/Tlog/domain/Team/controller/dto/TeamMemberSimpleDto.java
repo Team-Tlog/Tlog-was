e7f882697708f8038a3bb5342218c61d6dd77bdd
patternplane <bss011202@naver.com>
2025-10-13 23:00:17 +0900

    docs: TeamMemberSimpleDto 내 미흡한 문서화 보완

--- tlog-was/src/main/java/com/se/Tlog/domain/Team/controller/dto/TeamMemberSimpleDto.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Team/controller/dto/TeamMemberSimpleDto.java
@@ -2,11 +2,13 @@ package com.se.Tlog.domain.Team.controller.dto;
 
 
 import com.se.Tlog.domain.User.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.UUID;
 
 public record TeamMemberSimpleDto(
         UUID memberId,
+        @Schema(description = "팀원의 TBTI 유형입니다.", example = "RENA")
         String tbtiString,
         String profileImage
 ) {
