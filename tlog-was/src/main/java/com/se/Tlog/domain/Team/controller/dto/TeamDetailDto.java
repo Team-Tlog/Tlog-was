d4bbbcf52315d3917466a379904a03ed901e4f91
patternplane <bss011202@naver.com>
2025-10-30 01:36:23 +0900

    feat: 팀 상세 조회시 chatRootId 포함

--- tlog-was/src/main/java/com/se/Tlog/domain/Team/controller/dto/TeamDetailDto.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Team/controller/dto/TeamDetailDto.java
@@ -13,19 +13,21 @@ public record TeamDetailDto(
         String teamName,
         String tbtiString,
         String inviteCode,
+        Long chatRoomId,
         LocalDate createdAt,
         LocalDate expiredAt,
         List<TeamMemberDto> members,
         List<WishlistDestinationRes> wishlist,
         TravelPlanDto travelPlanDto
 ) {
-    public static TeamDetailDto from(Team team, List<TeamMemberDto> members,
+    public static TeamDetailDto from(Team team, Long chatRoomId, List<TeamMemberDto> members,
                                      List<WishlistDestinationRes> wishlist, TravelPlanDto travelPlanDto) {
         return new TeamDetailDto(
                 team.getId(),
                 team.getName(),
                 team.getTbtiString(), // 만약 TBTI 설명까지 추가해야 할 경우, 여기서 처리하지 않고 외부에서 설명 DTO를 파라미터로 받을 것!
                 InviteCodeUtil.longToStr(team.getInviteCode()),
+                chatRoomId,
                 team.getCreatedAt().toLocalDate(),
                 LocalDate.now(),
                 members,
