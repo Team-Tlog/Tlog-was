8307bfc01c4c342d8cdf25d8471859e3d3c7b0dc
gojungsu <gjs4565@naver.com>
2025-11-04 05:56:30 +0900

    feat: 메시지를 읽지않은 인원을 나타낼 수 있게끔 수정 및 메시지 id 누락부분 수정

--- tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/message/dto/ChatMessageDto.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Social/Chat/message/dto/ChatMessageDto.java
@@ -1,20 +1,28 @@
 package com.se.Tlog.domain.Social.Chat.message.dto;
 
 import com.se.Tlog.domain.Social.Chat.message.ChatMessage;
-import com.se.Tlog.domain.Social.Chat.room.ChatRoom;
-import com.se.Tlog.domain.User.domain.User;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record ChatMessageDto(
+        Long id,
         UUID senderId,
         String senderName,
         Long chatRoomId,
         String content,
-        LocalDateTime sendAt
+        LocalDateTime sendAt,
+        int unreadCount
 ) {
-    static public ChatMessageDto from(User user, ChatRoom chatRoom, ChatMessage chatMessage) {
-        return new ChatMessageDto(user.getId(), user.getName(), chatRoom.getId(), chatMessage.getContent(), chatRoom.getCreateAt());
+    public static ChatMessageDto from(ChatMessage message, int readCount, int totalParticipants) {
+        return new ChatMessageDto(
+                message.getId(),
+                message.getSender().getId(),
+                message.getSender().getName(),
+                message.getChatRoom().getId(),
+                message.getContent(),
+                message.getSendAt(),
+                totalParticipants - readCount
+        );
     }
 }
