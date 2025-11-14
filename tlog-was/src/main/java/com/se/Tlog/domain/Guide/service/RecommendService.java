ae061797400a74d8375a0a6563bb22d5ea19a8c1
patternplane <bss011202@naver.com>
2025-11-14 11:52:05 +0900

    feat: RecommendService에 변경된 DTO 반영

--- tlog-was/src/main/java/com/se/Tlog/domain/Guide/service/RecommendService.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/Guide/service/RecommendService.java
@@ -1,6 +1,7 @@
 package com.se.Tlog.domain.Guide.service;
 
 import com.se.Tlog.domain.ApplicationService;
+import com.se.Tlog.domain.Guide.controller.dto.BannerDestinationsDto;
 import com.se.Tlog.domain.Guide.controller.dto.BannerDto;
 import com.se.Tlog.domain.Guide.controller.dto.RecommendDestDto;
 import com.se.Tlog.domain.Guide.controller.dto.RecommendPostDto;
@@ -12,7 +13,7 @@ import com.se.Tlog.domain.Guide.repository.mongo.RecBannerRepository;
 import com.se.Tlog.domain.Guide.repository.mongo.RecDestRepository;
 import com.se.Tlog.domain.Guide.repository.mongo.RecPostRepository;
 import com.se.Tlog.domain.Travel.application.DestinationService;
-import com.se.Tlog.domain.Travel.controller.dto.DestinationSummaryRes;
+import com.se.Tlog.domain.Travel.domain.Destination;
 import com.se.Tlog.domain.User.domain.User;
 import com.se.Tlog.domain.User.repository.jpa.UserRepository;
 import com.se.Tlog.global.exception.CustomException;
@@ -34,13 +35,13 @@ public class RecommendService {
     private final RecDestRepository destRepository;
     private final RecPostRepository postRepository;
 
-    private BannerDto makeBannerDto(RecBanner banner, User user) {
+    private String makeTitle(RecBanner banner, User user) {
         if (BannerType.JUST_INFO == banner.getBannerType() ||
             BannerType.DESTINATION_BUNDLE == banner.getBannerType())
-            return BannerDto.create(banner);
+            return banner.getTitle();
         if (BannerType.TBTI_DEST_RECOMMEND == banner.getBannerType())
             // 추후 변경 계획 : 이 부분은 추천 알고리즘을 적용해, TBTI 및 계절에 따른 추천 여행지를 표시할 예정입니다.
-            return BannerDto.create(banner, user.getTbtiString() + banner.getTitle());
+            return user.getTbtiString() + banner.getTitle();
         else
             throw new CustomException(ErrorType.INTERNAL_SERVER_ERROR);
     }
@@ -50,7 +51,7 @@ public class RecommendService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(ErrorType.USER_NOT_FOUND));
         return banners.stream()
-                .map(ban -> makeBannerDto(ban, user))
+                .map(ban -> BannerDto.create(ban, makeTitle(ban, user)))
                 .toList();
     }
 
@@ -90,8 +91,13 @@ public class RecommendService {
                 .toList();
     }
 
-    public Page<DestinationSummaryRes> getBannerDests(String bannerId, Pageable pageable) {
-        return destinationService.convertToDto(
-                bannerRepository.findAllDestinationsFromBanner(bannerId, pageable));
+    public BannerDestinationsDto getBannerDests(UUID userId, String bannerId, Pageable pageable) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorType.USER_NOT_FOUND));
+        RecBanner banner = bannerRepository.findById(bannerId);
+        Page<Destination> destinations = bannerRepository.findAllDestinationsFromBanner(bannerId, pageable);
+        return new BannerDestinationsDto(
+                makeTitle(banner, user), // 25.11.14 추후 배너 제목 외에, 세부적으로 커스텀된 제목이 사용될 수 있습니다.
+                destinationService.convertToDto(destinations));
     }
 }
