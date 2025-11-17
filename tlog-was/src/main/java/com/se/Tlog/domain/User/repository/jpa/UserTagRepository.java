5008a6e6bccdcf824f007b8fd9cdb628ad6ccfac
patternplane <bss011202@naver.com>
2025-11-17 15:52:08 +0900

    feat: UserTagREpository 추가 쿼리 작성

--- tlog-was/src/main/java/com/se/Tlog/domain/User/repository/jpa/UserTagRepository.java
+++ tlog-was/src/main/java/com/se/Tlog/domain/User/repository/jpa/UserTagRepository.java
@@ -1,11 +1,14 @@
 package com.se.Tlog.domain.User.repository.jpa;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.se.Tlog.domain.User.domain.UserTagInfo;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserTagRepository extends JpaRepository<UserTagInfo, UUID> {
-
+    @Query("SELECT u.weight FROM UserTagInfo u WHERE u.user.id = :userid ORDER BY u.tagId ASC")
+    List<Double> findWeightsByUserId(UUID userid);
 }
